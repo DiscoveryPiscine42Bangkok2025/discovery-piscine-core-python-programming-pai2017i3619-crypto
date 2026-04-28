14ecc1f3c2a75af14ad2ff928a970d7cd1db3818
pai2017i3619-crypto <pai2017i3619@gmail.com>
2026-04-28 08:34:18 +0700

    Add files via upload

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,3 @@
+first_name = input("Hey, what's your first name?: ").strip()
+last_name = input("and your last name?: ").strip()
+print("well, pleased to meet you, " + first_name + " " + last_name + ".")
